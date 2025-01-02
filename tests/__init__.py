d5e24326aa666a2c1ed9e0dfb809b551e57de731
lballaty <liborballaty@gmail.com>
2025-01-02 12:38:24 +0100

    Update __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from .config import configure_app
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    configure_app(app)
+
+    with app.app_context():
+        from . import routes
+        return app
